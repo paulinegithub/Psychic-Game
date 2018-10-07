fdf20504b92a5b2437812052b5434350f4d57ad9
PLS <plsenh@gmail.com>
2018-10-07 14:11:30 -0400

    js correction

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,83 @@
 // declare variables to hold game information
-var winCount = 0;
-var loseCount = 0;
+var wins = 0;
+var losses = 0;
 var guessesLeft = 10;
-var guessList = "";
+var guessList = [];
 
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var userGuess = "";
 var answer = "";
 
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// var winText = document.getElementById("wins");
+// var lossText = document.getElementById("losses");
+// var chancesText = document.getElementById("guesses-left");
+// var listText = document.getElementById("guess-list");
+// console.log(winsText);
+// console.log(lossText);
+// console.log(chancesText);
+// console.log(listText);
 
+// function to get a random answer
+function updateAnswer() {
+    answer = letters[Math.floor(Math.random() * letters.length)];
+    console.log("target answer: " + answer);
+}
+
+// function to reset guesses left and guesses so far
+function resetGuess() {
+    guessesLeft = 10;
+    guessList = [];
+    console.log("guesses left: " + guessesLeft);
+    console.log("guesses list: " + guessList);
+}
 
 // generate a random letter from letters array for initial answer
-answer = letters[Math.floor(Math.random() * letters.length)];
-console.log("initial answer: " + answer);
+updateAnswer();
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function (event) {
 
-    userGuess = event.key.toUpperCase(); // convert to uppercase
+    userGuess = event.key.toUpperCase(); // convert keypress to uppercase
+    // console.log("keypress: " + event.key);
 
     // perform action only if userGuess is a letter
-    if (userGuess === letters[i]) {
-        
-    // -----UPDATE THIS-----------^    
+    if (letters.indexOf(userGuess) > -1) {
+        console.log("userGuess: " + userGuess);
 
         // determine if user has won - update counts for guesses and wins or losses
         if (userGuess === answer) {
-            winCount++; // increase win count
-            answer = letters[Math.floor(Math.random() * letters.length)]; // generate new random answer
-            console.log("answer after win: " + answer);
-            resetGuess;
-            update;
-        } else {
-            if (guessesLeft === 0) { // if no more guesses allowed
-                loseCount++; // increase losses
-                resetGuess;
-                update;
-            } else { // if there are guesses left
-                $("#guess-list").append(userGuess); // update list of guesses
-                // ------ FIX THIS --------------^
-                guessesLeft--;
-                update;
-            }
-            
+            alert("Correct! The answer was: " + answer);
+
+            wins++; // increase win count
+            console.log("wins: " + wins);
+
+            updateAnswer();
+            resetGuess();
+        } else { // if there are guesses left
+
+            guessList.push(userGuess); // update list of guesses
+            // console.log("guesses list: " + guessList);
+
+            guessesLeft--; // decrease guess count
+            // console.log("guesses left: " + guessesLeft);
         }
 
-        // function update score and guesses
-        var update = function () {
-        $("#win-count").text("Wins: " + winCount);
-        $("#lose-count").text("Losses: " + loseCount);
-        $("#guess-left").text("Guesses Left: " + guessesLeft);
-        $("#guess-list").text("Guesses so far: " + guessList);
+        // if no more guesses allowed
+        if (guessesLeft === 0) {
+
+            alert("Sorry, the correct answer was: " + answer);
+
+            losses++; // increase losses
+            console.log("losses: " + losses);
+
+            updateAnswer();
+            resetGuess();
         }
 
-        // function to reset guesses left and guesses so far
-        var resetGuess = function () {
-            guessesLeft = 10;
-            guessList = "";
-        }
+        // update score board
+        document.getElementById("wins").innerHTML = "Wins: " + wins;
+        document.getElementById("losses").innerHTML = "Losses: " + losses;
+        document.getElementById("guesses-left").innerHTML = "Guesses left: " + guessesLeft;
+        document.getElementById("guess-list").innerHTML = "Guesses so far: " + guessList;
     }
 }
